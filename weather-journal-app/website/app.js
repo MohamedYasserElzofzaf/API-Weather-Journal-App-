82388e023de733b873441da6b7bc9140108fe7c7
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-12 00:17:13 +0200

    add api key from the openweathermap.org

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
+const apiKey = "b6b2df7029c59742f6a82bb1b18c3456";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
