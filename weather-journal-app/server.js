5fef091d345f8bc027d0ce875441043abfae6cff
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-12 00:08:21 +0200

    add core use and options

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -9,14 +9,16 @@ const cors = require("cors");
 // Start up an instance of app
 const app = express();
 const port = 3000;
+const server = app.listen(port, listening);
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
 // Initialize the main project folder
 app.use(express.static("website"));
+app.use(cors());
+app.options("*", cors());
 
 // Setup Server
