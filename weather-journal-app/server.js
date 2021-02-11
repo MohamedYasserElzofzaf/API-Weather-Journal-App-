e80eb20ef7330bc9e1749cd706566aec869c008c
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-11 15:17:44 +0200

    add express to run server and routes && start up an instance of app

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,9 +2,13 @@
 let projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Start up an instance of app
-
+const app = express();
+const port = 3000;
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
