c589a343f0dbdb8d642a7daad42e3388e15dfb82
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-12 11:30:32 +0200

    add the event listener to the click button

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,6 +1,14 @@
+const { query } = require("express");
+
 /* Global Variables */
 const apiKey = "b6b2df7029c59742f6a82bb1b18c3456";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+const btn = document.querySelector("#generate");
+btn.addEventListener("click", () => {
+    const zipCode = document.querySelector("#zip").value;
+    getDate(zipCode);
+});
